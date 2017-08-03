00dd67c002bb86706d29ae12d3b2698444c88dae
jhyang12345 <jhyang12345@naver.com>
2017-08-03 23:28:03 +0900

    bug fixd for auto tagger

--- public/js/post.js
+++ public/js/post.js
@@ -11,46 +11,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
     taggerKeyup(evt);
   });
 
-  $(".tagger").on("keydown", function(evt) {
-    taggerKeydown(evt);
-
-  });
 });
 
 var keyCodes = [13, 32];
 
-function taggerKeydown(evt) {
-  console.log(evt.keyCode);
-  if(keyCodes.indexOf(evt.keyCode) >= 0) {
-    $(".post").append('<input type="text" class="tagger" placeholder="#" spellcheck="false"></input>');
-
-    $(".tagger").autoGrowInput({minWidth:10,comfortZone:3});
-
-    $(".tagger").trigger("update");
-
-    $(".tagger").last().focus();
-
-    $(".tagger").on("keydown", function(evt) {
-      taggerKeydown(evt);
-    });
-
-    $(".tagger").last().on("keyup", function(evt) {
-      taggerKeyup(evt);
-    });
-  }
-
-}
-
 function taggerKeyup(evt) {
   var buffer = $(".tagger").last().val().trim(" ");
   var actual = $(".tagger").last().val();
-  console.log(buffer.length);
-  console.log(actual.length);
-  if(buffer.length < actual.length) {
-    var evt = {keyCode: 32};
-    $(".tagger").last().val(buffer);
 
-    taggerKeydown(evt);
+  console.log(buffer);
+  if(buffer.length > 0 && (buffer.length < actual.length || keyCodes.indexOf(evt.keyCode) >= 0)) {
+    $(".tagger").last().val(buffer);
+    $(".tags_holder").append('<input type="text" class="tagger" placeholder="#" spellcheck="false"></input>');
+    $(".tagger").last().autoGrowInput({minWidth:10,comfortZone:3});
+    $(".tagger").last().trigger("update");
+    $(".tagger").last().focus();
 
     $(".tagger").last().on("keyup", function(evt) {
       taggerKeyup(evt);
